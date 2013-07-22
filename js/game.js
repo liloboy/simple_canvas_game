254a9c6aecff4fca86e26ce064e84f4ff36a803a
johan <johan@johan-Inspiron-1440.(none)>
2013-07-23 00:17:18 +0200

    Fixed the hero reset, so that the hero doesn't get moved to the middle of the canvas after each find.

--- js/game.js
+++ js/game.js
@@ -31,6 +31,8 @@ monsterImage.src = "images/monster.png";
 
 // Game objects
 var hero = {
+	x: canvas.width / 2,
+	y: canvas.height / 2,
 	speed: 256 // movement in pixels per second
 };
 var monster = {};
@@ -49,9 +51,6 @@ addEventListener("keyup", function (e) {
 
 // Reset the game when the player catches a monster
 var reset = function () {
-	hero.x = canvas.width / 2;
-	hero.y = canvas.height / 2;
-
 	// Throw the monster somewhere on the screen randomly
 	monster.x = 32 + (Math.random() * (canvas.width - 64));
 	monster.y = 32 + (Math.random() * (canvas.height - 64));
